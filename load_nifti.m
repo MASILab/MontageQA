3daead17be008831d52d3b9a10e9d2823b8811f4
ChangMarkusYU <marcus.yu.56@gmail.com>
2020-01-23 17:00:28 -0600

    intensity adjustment and rotation

--- load_nifti.m
+++ load_nifti.m
@@ -11,19 +11,20 @@ image_out = fullfile(out_folder, montage_name);
 
 addpath(masimatlab_path); 
 
-help load_untouch_nii_gz % prints out information about the function
+%my_nifti = load_untouch_nii_gz(img_path); % load the nifti
+% the "untouch" in the function name here mean
+header = niftiinfo(img_path);
+my_nifti = niftiread(header);
+my_nifti = imrotate3(my_nifti, 90, [0, 0, 1]);
+% may or may not be necessary to flip the image
+my_nifti = imrotate3(my_nifti, 180, [0, 1, 0]);
 
-my_nifti = load_untouch_nii_gz(img_path); % load the nifti
-% the "untouch" in the function name here means 
+fprintf('Nifti dimensions: %d x %d x %d\n', header.PixelDimensions); % look at the volume dimensions (my_nifti.hdr is the header)
 
-fprintf('Nifti dimensions: %d x %d x %d\n', my_nifti.hdr.dime.dim(2:4)); % look at the volume dimensions (my_nifti.hdr is the header)
-
-my_img_data = my_nifti.img; % gets the raw image data
-
-%figure(1); imagesc(my_img_data(:,:,15)); colormap(gray); % Display an axial slice of the CT
+% figure(1); imagesc(my_nifti(:,:,15)); colormap(gray); % Display an axial slice of the CT
 
 % make a montage using matlab's montage function
-m_handle = montage(my_img_data, 'DisplayRange',[]);
+m_handle = montage(my_nifti, 'DisplayRange',[920, 1110]);
 % save the montage to disk
 current_frame = getframe(gca);
 montage_uint16 = im2uint16(current_frame.cdata);
