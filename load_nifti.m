f07b5d2bcac9076a68924dc8d78ac8ba3cc2d1a5
ChangMarkusYU <marcus.yu.56@gmail.com>
2020-02-02 19:00:40 -0600

    improved labeling

--- load_nifti.m
+++ load_nifti.m
@@ -8,10 +8,16 @@ addpath(masimatlab_path);
 % path where the montage is saved
 out_folder = 'montage';
 
-image_path_list = readtable(csv_path, 'HeaderLines', 1);
-num_rows = height(image_path_list);
-score_list = image_path_list.(2);
-image_path_list = image_path_list.(1);
+image_path_table = readtable(csv_path, 'HeaderLines', 1);
+num_rows = height(image_path_table);
+score_list = image_path_table.(2);
+image_path_table = image_path_table.(1);
+image_path_list = strings(num_rows, 1);
+
+for i = 1:num_rows
+    path_cell = image_path_table(i);
+    image_path_list(i) = path_cell{1};
+end
 
 contrast = [1005, 1115];
 labels = zeros(num_rows, 3) - 1;
@@ -24,8 +30,7 @@ end
 
 while i <= num_rows
     if prev_i ~= i
-        path_cell = image_path_list(i);
-        nifti_path = path_cell{1};
+        nifti_path = image_path_list(i);
         res = makeMontage(nifti_path, out_folder, i, contrast, labels);
     end
     
@@ -45,17 +50,17 @@ function contrastButtonPushed(montage_handle, ~)
         @(s, e)getMinMax(s)); 
 end
 
- function getMinMax(handle)
-     window_min = str2double(get(findobj(handle, 'tag', ...
+function getMinMax(handle)
+    window_min = str2double(get(findobj(handle, 'tag', ...
          'window min edit'), 'String'));
-     window_max = str2double(get(findobj(handle, 'tag', ...
+    window_max = str2double(get(findobj(handle, 'tag', ...
          'window max edit'), 'String'));
-     assignin('base', 'contrast', [window_min, window_max]);
-     delete(handle);
- end
+    assignin('base', 'contrast', [window_min, window_max]);
+    delete(handle);
+end
  
- % label format: [T G B]
- function current_label = labelButtonPushed(col, current_label)
+% label format: [T G B]
+function current_label = labelButtonPushed(col, current_label)
     if current_label(col) == 0
         current_label(col) = 1;
     else
@@ -63,19 +68,19 @@ end
     end
     fprintf('[T: %d G: %d B: %d]\n', current_label(1), ...
         current_label(2), current_label(3));
- end
+end
 
- % TODO: add image path as the first column; Use writetable;
- %       preprocess image_path_list and turn it into a matrix
- function writeButtonPushed(i, current_label)
+function writeButtonPushed(i, current_label)
     labels = evalin('base', 'labels');
+    image_path_list = evalin('base', 'image_path_list');
     labels(i, :) = current_label;
-    writematrix(labels, 'testLabels.csv');
+    %writematrix(labels, 'testLabels.csv');
+    writetable(table(image_path_list, labels), 'testLabels.csv');
     assignin('base', 'labels', labels);
- end
+end
  
- function res = makeMontage(nifti_path, ...
-     out_folder, i, contrast, labels)
+function res = makeMontage(nifti_path, ...
+    out_folder, i, contrast, labels)
     nifti_path = strrep(nifti_path, 'preprocessed', 'nifti');
             
     nifti_header = niftiinfo(nifti_path);
@@ -97,10 +102,10 @@ end
     end
     
     res = {montage_handle, image_out};
- end
+end
  
- function index = getKeyboardInput(i, res, score_list, ...
-     num_rows, labels)
+function index = getKeyboardInput(i, res, score_list, ...
+    num_rows, labels)
     montage_handle = res(1);
     montage_handle = montage_handle{1};
     image_out = res(2);
@@ -151,5 +156,5 @@ end
             end
         end
     end
- end
+end
 
